7efaa7f7f3bdb11e4a1443c41944c46a6e6e42eb
Anum23432 <anumaneesshah@gmail.com>
2021-06-09 20:17:33 +0500

    Javascript Add Event Listener

---
+++ app.js
@@ -0,0 +1,23 @@
+function print() {
+
+let element = document.querySelector("h1").innerHTML = "TECH KARO";
+console.log(element);
+
+let a = document.querySelector("#first").style.border = "dotted";
+console.log(a);
+
+let b = document.querySelector("#first").style.color = "red";
+console.log(b);
+
+
+
+let c = document.getElementsByClassName("heading").className = "abc";
+console.log(c);
+
+let d = document.querySelector(".heading").style.color = "crimson";
+console.log(d);
+
+let e = document.querySelector("h2").classList.add("hji" , "efg");
+ let f = document.querySelector("h2").classList
+console.log(f);
+}
